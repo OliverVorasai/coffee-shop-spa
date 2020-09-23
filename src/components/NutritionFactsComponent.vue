445fa242a1c3d162cfe038e314acd223d0454046
olivervorasai <olivervorasai@gmail.com>
2020-09-23 16:18:05 -0700

    Added nutrition facts component

---
+++ src/components/NutritionFactsComponent.vue
@@ -0,0 +1,204 @@
+<template>
+  <b-container>
+    <b-row align-h="around" align-v="center">
+      <h1>{{ imageAlt }}</h1>
+      <b-img :src="imageSrc" fluid thumbnail></b-img>
+    </b-row>
+    <template v-if="nutritionInfo !== null">
+      <b-row>
+        <p><strong>Nutrition Information</strong></p>
+      </b-row>
+      <b-row class="row-border-bottom thick-hr">
+        <p>{{ nutritionInfo.serving.servingSize.name }} {{ nutritionInfo.serving.servingSize.amount}} {{ nutritionInfo.serving.servingSize.unit }}</p>
+      </b-row>
+      <b-row class="row-border-bottom" align-h="between">
+        <p>
+          <strong>{{ nutritionInfo.calories.calories.name }} {{ nutritionInfo.calories.calories.amount }}</strong>
+        </p>
+        <p>{{ nutritionInfo.calories.caloriesFromFat.name }} {{ nutritionInfo.calories.caloriesFromFat.amount }}</p>
+      </b-row>
+      <b-row class="row-border-bottom" align-h="end">
+        <p>% Daily Value *</p>
+      </b-row>
+      <b-row class="row-border-bottom" align-h="between">
+        <p>
+          <strong>{{ nutritionInfo.fat.totalFat.name }}</strong>
+          {{ nutritionInfo.fat.totalFat.amount }} {{ nutritionInfo.fat.totalFat.unit }}
+        </p>
+        <p>{{ nutritionInfo.fat.totalFat.percent }}%</p>
+      </b-row>
+      <b-row class="row-border-bottom" align-h="between">
+        <p class="ml-4">{{ nutritionInfo.fat.saturatedFat.name }} {{ nutritionInfo.fat.saturatedFat.amount }} {{ nutritionInfo.fat.saturatedFat.unit }}</p>
+        <p>{{ nutritionInfo.fat.saturatedFat.percent }}%</p>
+      </b-row>
+      <b-row class="row-border-bottom" align-h="between">
+        <p class="ml-4">{{ nutritionInfo.fat.transFat.name }} {{ nutritionInfo.fat.transFat.amount }} {{ nutritionInfo.fat.transFat.unit }}</p>
+      </b-row>
+      <b-row class="row-border-bottom" align-h="between">
+        <p>
+          <strong>{{ nutritionInfo.cholesterol.cholesterol.name }}</strong>
+          {{ nutritionInfo.cholesterol.cholesterol.amount }} {{ nutritionInfo.cholesterol.cholesterol.unit }}
+        </p>
+        <p>{{ nutritionInfo.cholesterol.cholesterol.percent }}%</p>
+      </b-row>
+      <b-row class="row-border-bottom" align-h="between">
+        <p>
+          <strong>{{ nutritionInfo.sodium.sodium.name }}</strong>
+          {{ nutritionInfo.sodium.sodium.amount }} {{ nutritionInfo.sodium.sodium.unit }}
+        </p>
+        <p>{{ nutritionInfo.sodium.sodium.percent }}%</p>
+      </b-row>
+      <b-row class="row-border-bottom" align-h="between">
+        <p>
+          <strong>{{ nutritionInfo.carbohydrates.totalCarbohydrates.name }}</strong>
+          {{ nutritionInfo.carbohydrates.totalCarbohydrates.amount }} {{nutritionInfo.carbohydrates.totalCarbohydrates.unit}}
+        </p>
+        <p>{{ nutritionInfo.carbohydrates.totalCarbohydrates.percent }}%</p>
+      </b-row>
+      <b-row class="row-border-bottom" align-h="between">
+        <p class="ml-4">{{ nutritionInfo.carbohydrates.fiber.name }} {{ nutritionInfo.carbohydrates.fiber.amount }} {{nutritionInfo.carbohydrates.fiber.unit}}</p>
+      </b-row>
+      <b-row class="row-border-bottom" align-h="between">
+        <p class="ml-4">{{ nutritionInfo.carbohydrates.sugar.name }} {{ nutritionInfo.carbohydrates.sugar.amount }} {{nutritionInfo.carbohydrates.sugar.unit}}</p>
+      </b-row>
+      <b-row class="row-border-bottom" align-h="between">
+        <p>
+          <strong>{{ nutritionInfo.protein.protein.name }}</strong>
+          {{ nutritionInfo.protein.protein.amount}} {{ nutritionInfo.protein.protein.unit }}
+        </p>
+        <p>{{ nutritionInfo.protein.protein.percent }}%</p>
+      </b-row>
+      <b-row class="row-border-bottom" align-h="between">
+        <p>
+          <strong>{{ nutritionInfo.caffeine.caffeine.name }}</strong>
+          {{ nutritionInfo.caffeine.caffeine.amount }} {{ nutritionInfo.caffeine.caffeine.unit }}
+        </p>
+      </b-row>
+    </template>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "NutritionFactsComponent",
+  props: {
+    imageSrc: String,
+    imageAlt: String,
+  },
+  data() {
+    return {
+      nutritionInfo: null,
+    };
+  },
+  mounted() {
+    this.nutritionInfo = {};
+    this.nutritionInfo = Object.assign({}, this.nutritionInfo, {
+      serving: {
+        servingSize: {
+          name: "Serving Size",
+          amount: 16,
+          unit: "fl oz",
+        },
+      },
+      calories: {
+        calories: {
+          name: "Calories",
+          amount: 240,
+        },
+        caloriesFromFat: {
+          name: "Calories From Fat",
+          amount: 40,
+        },
+      },
+      fat: {
+        totalFat: {
+          name: "Total Fat",
+          amount: 4.5,
+          unit: "g",
+          percent: 6,
+        },
+        saturatedFat: {
+          name: "Saturated Fat",
+          amount: 2,
+          unit: "g",
+          percent: 10,
+        },
+        transFat: {
+          name: "Trans Fat",
+          amount: 0,
+          unit: "g",
+        },
+      },
+      cholesterol: {
+        cholesterol: {
+          name: "Cholesterol",
+          amount: 20,
+          unit: "mg",
+          percent: 7,
+        },
+      },
+      sodium: {
+        sodium: {
+          name: "Sodium",
+          amount: 115,
+          unit: "mg",
+          percent: 5,
+        },
+      },
+      carbohydrates: {
+        totalCarbohydrates: {
+          name: "Total Carbohydrates",
+          amount: 45,
+          unit: "g",
+          percent: 16,
+        },
+        fiber: {
+          name: "Dietary Fiber",
+          amount: 0,
+          unit: "g",
+        },
+        sugar: {
+          name: "Sugars",
+          amount: 42,
+          unit: "g",
+        },
+      },
+      protein: {
+        protein: {
+          name: "Protein",
+          amount: 8,
+          unit: "g",
+          percent: 16,
+        },
+      },
+      caffeine: {
+        caffeine: {
+          name: "Caffeine",
+          amount: 95,
+          unit: "mg",
+        },
+      },
+    });
+  },
+};
+</script>
+
+<style scoped lang="scss">
+img {
+  max-width: 200px;
+  max-height: 200px;
+}
+
+p {
+  margin-bottom: 0;
+}
+
+.row-border-bottom {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  &.thick-hr {
+    border-bottom: 5px solid rgba(0, 0, 0, 0.1);
+  }
+  padding-top: 0.5em;
+  padding-bottom: 0.5em;
+}
+</style>
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -9,7 +9,9 @@
         <h3>{{ obj.groupName }}</h3>
         <b-row class="menu-items-alignment">
           <div v-for="(imageObj, index) in obj.images" :key="index">
-            <img :src="imageObj.path" />
+            <b-button class="menu-button" v-b-toggle.sidebar-right @click="handleClick">
+              <img :src="imageObj.path" :alt="imageObj.name" />
+            </b-button>
             <p>{{ imageObj.name }}</p>
           </div>
         </b-row>
@@ -23,57 +25,103 @@
         <h3>{{ obj.groupName }}</h3>
         <b-row class="menu-items-alignment">
           <div v-for="(imageObj, index) in obj.images" :key="index">
-            <img :src="imageObj.path" />
+            <b-button class="menu-button" v-b-toggle.sidebar-right>
+              <img :src="imageObj.path" :alt="imageObj.name" />
+            </b-button>
             <p>{{ imageObj.name }}</p>
           </div>
         </b-row>
       </b-col>
     </b-row>
+    <div>
+      <b-sidebar id="sidebar-right" title="Coffee Shop" right shadow :width="'90vw'">
+        <div class="px-3 py-2">
+          <NutritionFactsComponent
+            :imageSrc="sidebarInfo.imageSrc"
+            :imageAlt="sidebarInfo.imageAlt"
+          />
+        </div>
+      </b-sidebar>
+    </div>
   </b-container>
 </template>
 
 <script>
+import NutritionFactsComponent from "../components/NutritionFactsComponent";
+
 export default {
   name: "Menu",
+  components: {
+    NutritionFactsComponent,
+  },
   data() {
     return {
       drinks: [], // Array of Objects
       food: [], // Array of Objects
+      sidebarInfo: {},
     };
   },
   mounted() {
     // require.context cannot take a variable as path, so must do manually here
-    let coffeeContext = require.context("../assets/menu-items/drinks/coffee/", false, /\.svg$/);
-    let iceCoffeeContext = require.context("../assets/menu-items/drinks/ice-coffee/", false, /\.svg$/);
-    let frappeContext = require.context("../assets/menu-items/drinks/frappe/", false, /\.svg$/);
-    let teaContext = require.context( "../assets/menu-items/drinks/tea/", false, /\.svg$/);
-    let drinksOtherContext = require.context("../assets/menu-items/drinks/other/", false, /\.svg$/);
-    let cakeContext = require.context("../assets/menu-items/food/cake/", false, /\.svg$/);
-    let cookiesContext = require.context( "../assets/menu-items/food/cookies/", false, /\.svg$/);
-    let pastriesContext = require.context("../assets/menu-items/food/pastries/", false, /\.svg$/);
-    let foodOtherContext = require.context("../assets/menu-items/food/other/", false, /\.svg$/);
-    
+    let coffeeContext = require.context(
+      "../assets/menu-items/drinks/coffee/",
+      false,
+      /\.svg$/
+    );
+    let iceCoffeeContext = require.context(
+      "../assets/menu-items/drinks/ice-coffee/",
+      false,
+      /\.svg$/
+    );
+    let frappeContext = require.context(
+      "../assets/menu-items/drinks/frappe/",
+      false,
+      /\.svg$/
+    );
+    let teaContext = require.context(
+      "../assets/menu-items/drinks/tea/",
+      false,
+      /\.svg$/
+    );
+    let drinksOtherContext = require.context(
+      "../assets/menu-items/drinks/other/",
+      false,
+      /\.svg$/
+    );
+    let cakeContext = require.context(
+      "../assets/menu-items/food/cake/",
+      false,
+      /\.svg$/
+    );
+    let cookiesContext = require.context(
+      "../assets/menu-items/food/cookies/",
+      false,
+      /\.svg$/
+    );
+    let pastriesContext = require.context(
+      "../assets/menu-items/food/pastries/",
+      false,
+      /\.svg$/
+    );
+    let foodOtherContext = require.context(
+      "../assets/menu-items/food/other/",
+      false,
+      /\.svg$/
+    );
+
     // Note: Group name must be unique
-    this.drinks.push(this.loadImages2(coffeeContext, "Coffee"));
-    this.drinks.push(this.loadImages2(iceCoffeeContext, "Ice Coffee"));
-    this.drinks.push(this.loadImages2(frappeContext, "Frappe"));
-    this.drinks.push(this.loadImages2(teaContext, "Tea"));
-    this.drinks.push(this.loadImages2(drinksOtherContext, "Other Drinks"));
-    this.food.push(this.loadImages2(cakeContext,"Cake"));
-    this.food.push(this.loadImages2(cookiesContext,"Cookies"));
-    this.food.push(this.loadImages2(pastriesContext,"Pastries"));
-    this.food.push(this.loadImages2(foodOtherContext,"Other Foods"));
+    this.drinks.push(this.loadImages(coffeeContext, "Coffee"));
+    this.drinks.push(this.loadImages(iceCoffeeContext, "Ice Coffee"));
+    this.drinks.push(this.loadImages(frappeContext, "Frappe"));
+    this.drinks.push(this.loadImages(teaContext, "Tea"));
+    this.drinks.push(this.loadImages(drinksOtherContext, "Other Drinks"));
+    this.food.push(this.loadImages(cakeContext, "Cake"));
+    this.food.push(this.loadImages(cookiesContext, "Cookies"));
+    this.food.push(this.loadImages(pastriesContext, "Pastries"));
+    this.food.push(this.loadImages(foodOtherContext, "Other Foods"));
   },
   methods: {
-    loadImages(imageContext, store) {
-      imageContext.keys().forEach((key) => {
-        store.push({
-          name: key.slice(2, -4),
-          path: imageContext(key),
-        });
-      });
-    },
-    loadImages2(imageContext, groupName) {
+    loadImages(imageContext, groupName) {
       let obj = {};
       obj.groupName = groupName;
       obj.images = [];
@@ -84,15 +132,21 @@ export default {
         });
       });
       return obj;
-    }
+    },
+    handleClick(event) {
+      this.sidebarInfo = Object.assign({}, this.sidebarInfo, {
+        imageSrc: event.target.src,
+        imageAlt: event.target.alt,
+      });
+    },
   },
 };
 </script>
 
 <style scoped lang="scss">
-@import '../../node_modules/bootstrap/scss/functions';
-@import '../../node_modules/bootstrap/scss/variables';
-@import '../../node_modules/bootstrap/scss/mixins/breakpoints';
+@import "../../node_modules/bootstrap/scss/functions";
+@import "../../node_modules/bootstrap/scss/variables";
+@import "../../node_modules/bootstrap/scss/mixins/breakpoints";
 .menu-container {
   margin-top: 156px;
 }
@@ -100,13 +154,31 @@ export default {
 .menu-items-alignment {
   @include media-breakpoint-down(md) {
     justify-content: space-between;
+    div {
+      margin-bottom: 40px;
+    }
   }
-  
+
   @include media-breakpoint-up(md) {
     justify-content: start;
     div {
       margin-right: 80px;
     }
+  }
+}
+
+.menu-button {
+  background-color: transparent;
+  border-color: transparent;
+  border-radius: 25%;
+  &:not(:disabled):not(.disabled):active {
+    background-color: transparent;
+    border-color: transparent;
+    box-shadow: none;
+    filter: brightness(70%);
+  }
+  &:focus {
+    box-shadow: none;
   }
 }
 
